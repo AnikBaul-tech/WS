845337e2cb76661c4c4f6800d9d96974077a0d32
Anik Baul <anikbaul2@gmail.com>
2026-02-04 20:50:17 +0530

    SPORTZZ re-initialised

---
+++ Sportzz/src/index.js
@@ -0,0 +1,13 @@
+import express, { json } from "express"
+const app = express();
+const port = 3000
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send("Hello");
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on localhost:${port}`);
+})
